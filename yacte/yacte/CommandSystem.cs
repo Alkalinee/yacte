884d6c5eb5f3d983328a36ea59c1053f1035a441
adam.hellberg95 <adam.hellberg95@db4390d7-4e3c-92d1-feb1-9eaedae18ce6>
2010-12-18 20:36:21 +0000

    CHANGED: "Polished" the code a bit.

--- yacte/yacte/CommandSystem.cs
+++ yacte/yacte/CommandSystem.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 
 namespace yacte
 {
 	class CommandSystem
 	{
+		private const char _PREFIX = ':';
 	}
 }
