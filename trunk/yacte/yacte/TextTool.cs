de602ebf2e8a7bb64848b0098165b9f524dece5e
adam.hellberg95 <adam.hellberg95@db4390d7-4e3c-92d1-feb1-9eaedae18ce6>
2010-12-18 02:12:55 +0000

    ADDED: TextTool class, provides some methods to print lines, ascii art (NYI). CHANGED: Will now print horizontal lines above and below all file outputs.

---
+++ trunk/yacte/yacte/TextTool.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace yacte
+{
+	/// <summary>
+	/// Provides various methods to print horizontal lines and other "ascii-art".
+	/// </summary>
+	class TextTool
+	{
+		private readonly int CONSOLE_WIDTH = Console.WindowWidth;
+		public int p_CONSOLE_WIDTH
+		{
+			get { return CONSOLE_WIDTH; }
+		}
+
+		private readonly int CONSOLE_HEIGHT = Console.WindowHeight;
+		public int p_CONSOLE_HEIGHT
+		{
+			get { return CONSOLE_HEIGHT; }
+		}
+
+		public void PrintLine()
+		{
+			for(int i = 0; i < CONSOLE_WIDTH; i++)
+			{
+				Console.Write("-");
+			}
+			Console.WriteLine();
+		}
+	}
+}
