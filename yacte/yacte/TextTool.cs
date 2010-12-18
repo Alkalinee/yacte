217f3e4be964ec9d25b4d492140bba2314b2c446
adam.hellberg95 <adam.hellberg95@db4390d7-4e3c-92d1-feb1-9eaedae18ce6>
2010-12-18 02:24:01 +0000

    REMOVED: goto statement, using do-while loop instead (NEVER use goto in C*) CHANGED: TextTool.PrintSeperator --> TextTool.PrintSeparator (typo fix). ADDED: More documentation in the TextTool class.

--- yacte/yacte/TextTool.cs
+++ yacte/yacte/TextTool.cs
@@ -10,19 +10,28 @@ namespace yacte
 	/// </summary>
 	class TextTool
 	{
+		/// <summary>
+		/// The width of the console (the number of "columns")
+		/// </summary>
 		private readonly int CONSOLE_WIDTH = Console.WindowWidth;
 		public int p_CONSOLE_WIDTH
 		{
 			get { return CONSOLE_WIDTH; }
 		}
 
+		/// <summary>
+		/// The height of the console (the number of "rows")
+		/// </summary>
 		private readonly int CONSOLE_HEIGHT = Console.WindowHeight;
 		public int p_CONSOLE_HEIGHT
 		{
 			get { return CONSOLE_HEIGHT; }
 		}
 
-		public void PrintSeperator()
+		/// <summary>
+		/// Prints a horizontal line (----) with a newline.
+		/// </summary>
+		public void PrintSeparator()
 		{
 			for(int i = 0; i < CONSOLE_WIDTH; i++)
 			{
