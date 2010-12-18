ea991e01877f94714ab841a1534de990a691696e
adam.hellberg95 <adam.hellberg95@db4390d7-4e3c-92d1-feb1-9eaedae18ce6>
2010-12-18 02:59:52 +0000

    (TextTool.cs) ADDED: Replace method for use with a search-and-replace feature.

--- trunk/yacte/yacte/TextTool.cs
+++ trunk/yacte/yacte/TextTool.cs
@@ -6,7 +6,7 @@ using System.Text;
 namespace yacte
 {
 	/// <summary>
-	/// Provides various methods to print horizontal lines and other "ascii-art".
+	/// Provides various methods to manipulate text.
 	/// </summary>
 	class TextTool
 	{
@@ -35,6 +35,32 @@ namespace yacte
 		{
 			for(int i = 0; i < CONSOLE_WIDTH; i++)
 				Console.Write("-");
+		}
+
+		/// <summary>
+		/// Replaces a word or string with another string.
+		/// </summary>
+		/// <param name="content">The string to be replaced.</param>
+		/// <param name="search">Original string.</param>
+		/// <param name="replace">The string to replace all current occurrences of "search".</param>
+		/// <returns>The new string with replaced strings if successful, null if failed.</returns>
+		public string Replace(string content, string search, string replace)
+		{
+			Console.WriteLine("Replacing \"" + search + "\" with \"" + replace + "\".");
+			if (content.Contains(search))
+			{
+				try
+				{
+					content = content.Replace(search, replace);
+					Console.WriteLine("Successfully replaced word!");
+					return content;
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine("Error replacing text, exception: " + ex.Message + "\n==" + ex.Source + "==");
+				}
+			}
+			return null;
 		}
 	}
 }
