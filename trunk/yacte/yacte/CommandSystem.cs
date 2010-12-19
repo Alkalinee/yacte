e5108bb07d0499f4f4af7059d8d39af00cd92810
adam.hellberg95 <adam.hellberg95@db4390d7-4e3c-92d1-feb1-9eaedae18ce6>
2010-12-19 03:22:59 +0000

    REMOVED: Some using statements. REMOVED: Old comments.

--- trunk/yacte/yacte/CommandSystem.cs
+++ trunk/yacte/yacte/CommandSystem.cs
@@ -1,10 +1,104 @@
 ﻿using System;
-using System.Text;
+using System.IO;
 
 namespace yacte
 {
 	class CommandSystem
 	{
+		readonly TextFile textFile = new TextFile();
+		readonly TextTool tt = new TextTool();
+
 		private const char _PREFIX = ':';
+
+		private const string _QUIT = "q";
+		private const string _OPEN = "o";
+		private const string _SAVE = "s";
+		private const string _REPLACE = "r";
+		private const string _LIST = "l";
+
+		private string fileName;
+
+		public CommandSystem(string[] clArgs)
+		{
+			int numArgs = clArgs.Length;
+			fileName = "";
+			if (numArgs > 0)
+				if (!string.IsNullOrEmpty(clArgs[0]))
+					fileName = clArgs[0];
+		}
+
+		public bool IsCommand(string line)
+		{
+			if (line.StartsWith(_PREFIX.ToString()))
+				return true;
+			return false;
+		}
+
+		public void HandleInput(string line)
+		{
+			if (IsCommand(line))
+			{
+				line = line.TrimStart(_PREFIX);
+				string args = line.Substring(1);
+				string command = line.Substring(0, 1);
+
+				switch (command)
+				{
+					case _QUIT:
+						Console.WriteLine("kthxbai");
+						Environment.Exit(0);
+						break;
+					case _SAVE:
+						if (!string.IsNullOrEmpty(fileName))
+							textFile.SaveFile(fileName.Trim(), false);
+						else
+							Console.WriteLine("Error: No file is open, please open a file with \":o <fileName>\" before saving.");
+						break;
+					case _REPLACE:
+						//TODO: Add replace code here
+						break;
+					case _OPEN:
+						textFile.Wipe();
+						if (!string.IsNullOrEmpty(args))
+						{
+							fileName = args.Trim();
+							if (File.Exists(fileName))
+							{
+								textFile.ReadFile(fileName);
+							}
+							else
+							{
+								Console.WriteLine("File \"" + fileName + "\" does not exist. It will be created when you save.");
+							}
+						}
+						else
+						{
+							Console.WriteLine("Error: No filename specified.");
+						}
+						break;
+					case _LIST:
+						//TODO: List commands here
+						break;
+					default:
+						//TODO: Print a list of commands
+						Console.WriteLine("Invalid command: \"" + command + "\"");
+						break;
+				}
+			}
+			else
+			{
+				if (!string.IsNullOrEmpty(fileName))
+				{
+					textFile.WriteContent(line, true);
+					tt.PrintSeparator();
+					textFile.ReadContent();
+					tt.PrintSeparator();
+				}
+				else
+				{
+					Console.WriteLine("Please open a file with \":o <fileName>\" before trying to write.");
+				}
+			}
+		}
 	}
 }
---
+++ trunk/yacte/yacte/TextFile.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.IO;
+
+namespace yacte
+{
+	class TextFile
+	{
+		private bool IsReading;
+		private bool IsWriting;
+		private string fileContent = "";
+		private TextReader _fileRead;
+		private TextWriter _fileWrite;
+
+		private void LoadFile(bool writeMode, string fileName, bool append)
+		{
+			Console.WriteLine("Opening file \"" + fileName + "\" for " + (writeMode ? "writing" : "reading"));
+			try
+			{
+				if (!writeMode)
+				{
+					if (!IsWriting)
+					{
+						_fileRead = new StreamReader(fileName);
+						IsReading = true;
+					}
+					else
+					{
+						Console.WriteLine("File is open for writing. Please close it first.");
+						return;
+					}
+				}
+				else
+				{
+					if (!IsReading)
+					{
+						_fileWrite = new StreamWriter(fileName, append);
+						IsWriting = true;
+					}
+					else
+					{
+						Console.WriteLine("File is open for reading. Please close it first.");
+						return;
+					}
+				}
+				Console.WriteLine("Successfully opened file!");
+			}
+			catch(Exception ex)
+			{
+				Console.WriteLine("Error opening file: " + ex.Message + "\n==" + ex.Source + "==");
+			}
+		}
+
+		private void CloseFile()
+		{
+			try
+			{
+				if (IsReading && IsWriting)
+				{
+					_fileRead.Close();
+					_fileWrite.Flush();
+					_fileWrite.Close();
+					IsReading = false;
+					IsWriting = false;
+					return;
+				}
+				if (IsReading)
+				{
+					_fileRead.Close();
+					IsReading = false;
+					return;
+				}
+				if (IsWriting)
+				{
+					_fileWrite.Flush();
+					_fileWrite.Close();
+					IsWriting = false;
+					return;
+				}
+				Console.WriteLine("No files open.");
+				return;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Error closing file: " + ex.Message + "\n==" + ex.Source + "==");
+				return;
+			}
+		}
+
+		public void WriteContent(string content, bool append)
+		{
+			try
+			{
+				if (append)
+					fileContent += Environment.NewLine + content;
+				else
+					fileContent = content;
+				Console.WriteLine("Successfully added text!");
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Error adding text: " + ex.Message + "\n==" + ex.Source + "==");
+			}
+		}
+
+		public void SaveFile(string fileName, bool append)
+		{
+			try
+			{
+				if (IsReading)
+				{
+					Console.WriteLine("File is open for reading, please close it before writing.");
+					return;
+				}
+				LoadFile(true, fileName, append);
+				_fileWrite.Write(fileContent);
+				CloseFile();
+				Console.WriteLine("Successfully wrote to file!");
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Error writing to file: " + ex.Message + "\n==" + ex.Source + "==");
+			}
+		}
+
+		public void ReadContent()
+		{
+			Console.WriteLine(fileContent);
+		}
+
+		public void ReadFile(string fileName)
+		{
+			if (IsWriting)
+			{
+				Console.WriteLine("File is open for writing, please close it before reading.");
+				return;
+			}
+			try
+			{
+				TextTool tt = new TextTool();
+				LoadFile(false, fileName, true);
+				fileContent = _fileRead.ReadToEnd();
+				CloseFile();
+				tt.PrintSeparator();
+				Console.WriteLine(fileContent);
+				tt.PrintSeparator();
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Error when reading file: " + ex.Message + "\n==" + ex.Source + "==");
+			}
+		}
+
+		public void Wipe()
+		{
+			fileContent = "";
+		}
+	}
+}
